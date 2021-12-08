56069f4291e0f6a215aa3330cff2319fd644b426
Joaquin Navarro <jonavcar@gmail.com>
2021-12-08 16:14:52 -0500

    Proyecto4

---
+++ src/test/java/com/banck/discovery/RegistreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banck.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
